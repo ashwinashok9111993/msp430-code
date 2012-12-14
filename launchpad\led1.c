871bb5219ccd4a6a3e7b24c21459524db6ffe711
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2012-12-13 19:49:41 -0800

    Create launchpad\led1.c

---
+++ launchpad\led1.c
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------*/
+// MSP430G2231 helloworld program
+//        |=======|
+// P1.0<--|       |
+//        |       |
+//        |       |
+//        |=======|
+//
+// red led connected to P1.0
+// written by ASHOK ASHWIN
+/*------------------------------------------------------------------*/
+
+#include <msp430g2231.h> //include file
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD; // stop the watchdog timer from chip reset
+  
+	P1DIR = 0b00000001;       // set direction of P1.0 to output
+	while(1)                  // endless loop
+	{
+		P1OUT = 0b00000001;     //set P1.0 to high
+	}
+}
