13f86f17ab6a5cf0cd6fe0f856aef22536d99900
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2013-09-14 18:01:04 +0530

    Create pwm.c

---
+++ launchpad/pwm.c
@@ -0,0 +1,24 @@
+#include <msp430.h>
+ 
+void main(void)
+{
+ WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+ P1DIR |= 0x0C; // P1.2 and P1.3 output
+ P1SEL |= 0x0C; // P1.2 and P1.3 TA1/2 options
+ TACCR0 = 1024; // PWM Period
+ TACCTL1 = OUTMOD_7; // CCR1 reset/set
+ TACCR1 = 20; // CCR1 PWM duty cycle
+ TACTL = TASSEL_2 + MC_1 + TAIE; // ACLK, up mode
+ 
+ ADC10CTL0 = ADC10SHT_2 + ADC10ON ; // ADC10ON, interrupt enabled
+ ADC10CTL1 = INCH_5; // input A1
+ 
+ 
+for (;;)
+ {
+ ADC10CTL0 |= ENC + ADC10SC; // Sampling and conversion start
+ while (ADC10CTL1 & ADC10BUSY); // ADC10BUSY?
+ TACCR1 = ADC10MEM;
+ }
+}
+
